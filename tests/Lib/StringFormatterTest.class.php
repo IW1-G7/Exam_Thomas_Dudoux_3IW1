224e7ca8d2ae5751615daba39dc6bbc2a7ebc5a1
Thomas DUDOUX <thomas.dudoux@gmail.com>
2017-02-01 16:15:11 +0100

    Start of the exercice

--- tests/Lib/StringFormatterTest.class.php
+++ tests/Lib/StringFormatterTest.class.php
@@ -6,4 +6,32 @@
 
   class StringFormatterTest extends \PHPUnit_Framework_TestCase {
 
+    public function testPrefix() {
+
+      // Déclaration de la classe
+      $string = new StringFormatter();
+
+    }
+
+    public function testSuffix() {
+
+      // Déclaration de la classe
+      $string = new StringFormatter();
+
+    }
+
+    public function testtoCamelCase() {
+
+      // Déclaration de la classe
+      $string = new StringFormatter();
+
+    }
+
+    public function testConcatString() {
+
+      // Déclaration de la classe
+      $string = new StringFormatter();
+
+    }
+
   }
